1af73c7c000a2b89ca78ec7b1ad429ea336a9be2
Basia Kmiecik <basiakmiecik@gmail.com>
2018-09-22 21:26:45 +0200

    dodanie zadania 1 i zadania 2

---
+++ src/zadanie1/NameArray.java
@@ -0,0 +1,13 @@
+package zadanie1;
+
+import java.util.Arrays;
+
+public class NameArray {
+
+    public static void main(String[] args) {
+
+            String[] tab= new String[]{"Basia", "Kasia", "Asia"};
+            String arrayString= Arrays.toString(tab);
+            System.out.println(arrayString);
+    }
+}
---
+++ src/zadanie2/AddArray.java
@@ -0,0 +1,10 @@
+package zadanie2;
+
+public class AddArray {
+    public static void main(String[] args) {
+        int tab[]= new int[]{3,6,8,5,3};
+        int sum= tab[0]+tab[2]+tab[4];
+        System.out.println("Suma z dodania pierwszej liczby ("+tab[0]+"), trzeciej liczby ("+tab[2]+") " +
+                "i piątej liczby ("+tab[4]+") wynosi: "+sum);
+    }
+}

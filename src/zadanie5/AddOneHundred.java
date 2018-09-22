863f42172e0777f4d164ff5065905f4cd3bb2598
Basia Kmiecik <basiakmiecik@gmail.com>
2018-09-22 23:03:01 +0200

    poprawione zadanie5

--- src/zadanie5/AddOneHundred.java
+++ src/zadanie5/AddOneHundred.java
@@ -2,11 +2,13 @@ package zadanie5;
 
 public class AddOneHundred {
     public static void main(String[] args) {
-
+            int sum=0;
         for (int i = 0; i < 101; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
+                sum+=i;
             }
         }
+        System.out.println("Suma liczb parzystych wynosi: "+sum);
     }
 }
